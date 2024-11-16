e75d7d8c321d7a9ba5cf601a37ffaceee40281bd
Salami Azeez <salamitopeazeez@gmail.com>
2024-11-16 15:01:47 +0100

    Completed Hero section

--- src/components/heroSection.jsx
+++ src/components/heroSection.jsx
@@ -1,8 +1,19 @@
 
 export const HeroSection = () => {
   return (
-    <div className="bg-darkGreen h-[200vh]">
-
+    <div className="h-[521px] text-center  flex flex-col items-center justify-center text-white">
+        <h1 className="text-[72px] font-bold leading-[88.56px]">
+          Exceptional Agricultural <br />
+          Commodities
+        </h1>
+        <p className="text-[28px] pt-5">
+          We Lead in Agricultural Commodities supply. <br />
+           Nature's Best, delivered with Excellence. 
+        </p>
+        <div className="flex gap-5 mt-3">
+          <button className="btn bg-white text-black hover:border-[2px] hover:text-white hover:border-white hover:bg-transparent">Get in touch!</button>
+          <button className="btn border-white border-[2px] hover:bg-white hover:text-darkGreen">View products</button>
+        </div>
     </div>
   )
 }
