f3794f831a28e5dfc94025b8fa251ba20b7b9dd9
Salami Azeez <salamitopeazeez@gmail.com>
2024-11-16 17:14:57 +0100

    Adjusted padding for hero section

--- src/components/heroSection.jsx
+++ src/components/heroSection.jsx
@@ -2,7 +2,8 @@ import left from "../assets/hero-left.jpeg"
 import { ImgHero } from "./imgHero"
 export const HeroSection = () => {
   return (
-    <div className="text-center mt-[100px] px-10 flex flex-col gap-5 items-center justify-center text-white md:gap-0 md:mt-0">
+    <div className="text-center mt-[100px] text-white md:gap-0 md:mt-0">
+      <div className="flex flex-col gap-5 items-center justify-cente px-10">
         {/* ============== First Section =========== */}
         <h1 className="text-6xl font-bold md:leading-[88.56px] md:text-[72px]">
           Exceptional Agricultural <span className="hidden md:inline"><br /></span>
@@ -10,7 +11,7 @@ export const HeroSection = () => {
         </h1>
         <p className="text-xl pt-5 md:text-[28px]">
           We Lead in Agricultural Commodities supply. <span className="hidden md:inline"><br /></span>
-           Nature's Best, delivered with Excellence. 
+          Nature's Best, delivered with Excellence.
         </p>
         <div className="flex flex-col gap-5 mt-5 md:flex-row">
           <button className="btn bg-white text-black hover:border-[2px] hover:text-white hover:border-white hover:bg-transparent">Get in touch!</button>
@@ -18,6 +19,7 @@ export const HeroSection = () => {
         </div>
         {/* ============== Second section ================= */}
         <ImgHero />
+      </div>
     </div>
   )
 }
