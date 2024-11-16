12c7fa023eda5d6390875a380595e76485fce756
Salami Azeez <salamitopeazeez@gmail.com>
2024-11-16 17:08:36 +0100

    Adjusted font size for hero section for mobile

--- src/components/heroSection.jsx
+++ src/components/heroSection.jsx
@@ -4,15 +4,15 @@ export const HeroSection = () => {
   return (
     <div className="text-center mt-[100px] px-10 flex flex-col gap-5 items-center justify-center text-white md:gap-0 md:mt-0">
         {/* ============== First Section =========== */}
-        <h1 className="text-7xl font-bold md:leading-[88.56px] md:text-[72px]">
+        <h1 className="text-6xl font-bold md:leading-[88.56px] md:text-[72px]">
           Exceptional Agricultural <span className="hidden md:inline"><br /></span>
           Commodities
         </h1>
-        <p className="text-2xl pt-5 md:text-[28px]">
+        <p className="text-xl pt-5 md:text-[28px]">
           We Lead in Agricultural Commodities supply. <span className="hidden md:inline"><br /></span>
            Nature's Best, delivered with Excellence. 
         </p>
-        <div className="flex gap-5 mt-5">
+        <div className="flex flex-col gap-5 mt-5 md:flex-row">
           <button className="btn bg-white text-black hover:border-[2px] hover:text-white hover:border-white hover:bg-transparent">Get in touch!</button>
           <button className="btn border-white border-[2px] hover:bg-white hover:text-darkGreen">View products</button>
         </div>
