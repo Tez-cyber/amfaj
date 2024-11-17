3014bed71e1360fa02a2d4bb864863ac1acd037b
Salami Azeez <salamitopeazeez@gmail.com>
2024-11-17 09:24:56 +0100

    Added utils file with tabsData

---
+++ src/utils.js
@@ -0,0 +1,20 @@
+
+// ============== tabs data
+export const tabsData = [
+    {
+        nav: "Sesame",
+        mainHead: "Sesame Seeds",
+        desc: `
+            We are one of the top exporters of this commodity from Nigeria, 
+            Our cutting edge facilities allows us to provide natural sesame 
+            seeds that are cleaned and fulfill consumer specifications.
+        `,
+        specs: [
+            "Purity: 99% Min",
+            "Admixture: Less than .5-1%",
+            "Moisture: Less than 6%",
+            "Oil content: 52% Min",
+            "FFA: 2% Maximum"
+        ]
+    }
+]
