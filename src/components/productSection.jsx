66cc9f945cf3d477dc5077117edd989ded1b1721
Salami Azeez <salamitopeazeez@gmail.com>
2024-11-17 18:39:46 +0100

    Completed tab for productSection

--- src/components/productSection.jsx
+++ src/components/productSection.jsx
@@ -1,7 +1,15 @@
-import { tabsData } from "../utils"
+import { useState } from "react"
+import { tabNav, tabsData } from "../utils"
+import { motion, useAnimate } from "framer-motion"
 
 export const ProductSection = () => {
+    const [tabItem, settabItem] = useState(1)
     const headStyle = "text-[#016623] pb-6 text-[35px]"
+
+    const id = tabsData.filter(tab => {
+        return tab.id
+    })
+
 
     return (
         <section className="flex flex-col my-20 mx-10 gap-10 bg-white p-5  rounded-2xl md:mx-28">
@@ -10,14 +18,25 @@ export const ProductSection = () => {
             </div>
             {/* ================= Tab section ============= */}
             <section className="">
+                {/* ====== tab nav ====== */}
+                <div
+                    className="mb-10">
+                    {
+                        tabNav.map((nav) => (
+                            <motion.button key={nav.id}
+                                transition={{ duration: .5 }}
+                                whileTap={{ y: 30 }}
+                                onClick={() => {
+                                    settabItem(nav.id)
+                                }}
+                                className={tabItem === nav.id ? "bg-lime mr-5 p-1 px-3 rounded-lg text-darkGreen font-semibold text-[18px]" : "transition duration-150 ease-in mr-5 cursor-pointer text-[18px] text-darkGreen font-semibold mb-10"}>{nav.name}</motion.button>
+                        ))
+                    }
+                </div>
                 {
                     tabsData.map((tab) => (
-                        <main key={tab.id}>
-                            {/* ====== tab nav ====== */}
-                            <div className="">
-                                <p className="text-[18px] text-darkGreen font-semibold">{tab.nav}</p>
-                            </div>
-                            <section className="flex flex-col md:gap-10 md:flex-row">
+                        <main key={tab.id} className={tabItem === tab.id ? "block" : "hidden"}>
+                            <section className="flex flex-col justify-center items-center mb-5 md:gap-10 md:flex-row">
                                 <section className="pb-10 md:w-[45%] md:pb-0">
                                     {/* ======= display section ======= */}
                                     <div className="">
@@ -27,8 +46,11 @@ export const ProductSection = () => {
                                             <h2 className="text-2xl font-[500] pb-5">Specifications</h2>
                                             {
                                                 tab.specs.map(spec => (
-                                                    <div className="">
-                                                        <li>{spec}</li>
+                                                    <div key={spec} className="">
+                                                        <li>
+                                                            <span className="inline-block mr-3 rotate-45 size-2 bg-[#9ace12]"></span>
+                                                            {spec}
+                                                        </li>
                                                     </div>
                                                 ))
                                             }
@@ -36,9 +58,14 @@ export const ProductSection = () => {
                                     </div>
                                 </section>
                                 {/* ============== image section =========== */}
-                                <section className="md:w-1/2 ">
-                                    <img src={tab.img} alt="" />
-                                </section>
+                                <motion.section
+                                    initial={{ opacity: 0, scale: 0.9 }}
+                                    animate={{ opacity: 1, scale: 1 }}
+                                    exit={{ opacity: 0, scale: 0.9 }}
+                                    transition={{ duration: .5 }}
+                                    className="md:w-1/2 ">
+                                    <img className="rounded-2xl" src={tab.img} alt="" />
+                                </motion.section>
                             </section>
                         </main>
                     ))
--- src/utils.js
+++ src/utils.js
@@ -1,10 +1,18 @@
 
 // ============== tabs data
+export const tabNav = [
+    { id: 1, name: "Sesame" },
+    { id: 2, name: "Cashew (RCN)" },
+    { id: 3, name: "Hibiscus flower" },
+    { id: 4, name: "Ginger" },
+    { id: 5, name: "Cocoa" },
+    { id: 6, name: "Cassia Tora" },
+]
+
 export const tabsData = [
     {
         id: 1,
         img: "/sesame.png",
-        nav: "Sesame",
         mainHead: "Sesame Seeds",
         desc: `
             We are one of the top exporters of this commodity from Nigeria, 
@@ -17,6 +25,102 @@ export const tabsData = [
             "Moisture: Less than 6%",
             "Oil content: 52% Min",
             "FFA: 2% Maximum"
+        ]
+    },
+    {
+        id: 2,
+        img: "/cashew.png",
+        nav: "",
+        mainHead: "Cashew (RCN)",
+        desc: `
+            We are committed to supply raw cashew nuts having good quality yield.
+        `,
+        specs: [
+            "Moisture: 10% Maximum",
+            "Admixture: 2% Maximum",
+            "Total Defective: Less than 5%",
+            "Nut Count: Between 180 – 205 nuts/kg (Kogi Quality)",
+            "Nut Count: Between 170 – 190 nuts/kg (Ogbomosho",
+            "KOR (Kernel Output Ratio): 48lbs Plus"
+        ]
+    },
+    {
+        id: 3,
+        img: "/hibiscus.png",
+        nav: "",
+        mainHead: "Hibiscus flower",
+        desc: `
+            We have a comprehensive facility for supplying freshly cleaned hibiscus 
+            flowers. We have a continuous record of high-quality Hibiscus flowers 
+            that meet the following specifications:
+        `,
+        specs: [
+            "Color: Bright Red/Dark Red",
+            "Purity: More than 98%",
+            "Moisture content: 8%–10%",
+            "Full Flower: 85-90%",
+            "Foreign matter: Less than 0.5%"
+        ]
+    },
+    {
+        id: 4,
+        img: "/ginger.png",
+        nav: "",
+        mainHead: "Ginger",
+        desc: `
+           We provide the finest quality Dried Split Ginger. Our highly qualified crew 
+           sources, processes, and inspects, ensuring that the cleaning is done properly 
+           and the finished product fulfills international standards.
+        `,
+        specs: [
+            "Hand Picked: European Standard ( Big pieces of whitish ginger – No Roots, No Mosels, No Powder Full of Natural Aroma)",
+            "ASTA quality: (No Roots, No Mosel, No Powder Full of natural Aroma)",
+            "Normal Quality: Best Quality whitish Ginger (Root, Mosel, powder Max 5%)",
+            "Moisture: Max 10%",
+            "Oil content: Max 2%",
+            "Impurities: Less than 1%",
+            "Color: Whitish",
+            "Packaging: 40kg pp white bags"
+        ]
+    },
+    {
+        id: 5,
+        img: "/cocoa.png",
+        nav: "",
+        mainHead: "Cocoa",
+        desc: `
+           The cocoa bean, also called cacao bean, cocoa and cacao is the dried and fully fermented seed of Theobroma cacao, from which cocoa solids.
+        `,
+        specs: [
+            "Hand Picked: European Standard ( Big pieces of whitish ginger – No Roots, No Mosels, No Powder Full of Natural Aroma)",
+            "ASTA quality: (No Roots, No Mosel, No Powder Full of natural Aroma)",
+            "Normal Quality: Best Quality whitish Ginger (Root, Mosel, powder Max 5%)",
+            "Moisture: Max 10%",
+            "Oil content: Max 2%",
+            "Impurities: Less than 1%",
+            "Color: Whitish",
+            "Packaging: 40kg pp white bags"
+        ]
+    },
+    {
+        id: 6,
+        img: "/cassie.png",
+        nav: "",
+        mainHead: "Cassie Tora",
+        desc: `
+           We provide the finest quality Dried Split Ginger. Our highly qualified crew 
+           sources, processes, and inspects, ensuring that the cleaning is done properly 
+           and the finished product fulfills international standards.
+        `,
+        specs: [
+            "Hand Picked: European Standard ( Big pieces of whitish ginger – No Roots, No Mosels, No Powder Full of Natural Aroma)",
+            "ASTA quality: (No Roots, No Mosel, No Powder Full of natural Aroma)",
+            "Normal Quality: Best Quality whitish Ginger (Root, Mosel, powder Max 5%)",
+            "Moisture: Max 10%",
+            "Oil content: Max 2%",
+            "Impurities: Less than 1%",
+            "Color: Whitish",
+            "Packaging: 40kg pp white bags"
         ]
     }
 ]
