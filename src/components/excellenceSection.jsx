deaba878f120e4d468ff394c8bbdad310090d270
Salami Azeez <salamitopeazeez@gmail.com>
2024-11-17 08:40:32 +0100

    Made card section responsive

--- src/components/excellenceSection.jsx
+++ src/components/excellenceSection.jsx
@@ -3,14 +3,14 @@ import thumbs from "../assets/thumbs.png"
 import energy from "../assets/energy.png"
 
 export const ExcellenceSection = () => {
-    const headStyle = "text-[#016623] font-semibold pb-6 md:text-[56px]"
+    const headStyle = "text-[#016623] pb-6 text-3xl "
     const card = "px-5 py-10 rounded-2xl "
     const cardHead = "capitalize pb-3 md:text-[24px]"
     const cardImg = "py-4"
     return (
-        <section className="flex flex-col justify-center items-center my-10 gap-10 bg-white p-5 rounded-2xl md:mx-28">
+        <section className="flex flex-col justify-center items-center m-10 gap-10 bg-white p-5 rounded-2xl md:mx-28">
             <div className="text-center">
-                <h2 className={headStyle}>Excellence meets Integrity</h2>
+                <h2 className={`${headStyle} font-semibold md:text-[56px]`}>Excellence meets Integrity</h2>
                 <p className="text-[#3A3A3C] md:px-10 md:text-[24px]">
                     Established with a dedication to excellence and integrity, AM FAJ Global is committed to
                     delivering top-quality products and unparalleled service to our valued customers across
@@ -20,8 +20,8 @@ export const ExcellenceSection = () => {
             </div>
             {/* ============ Card section ============= */}
             <section className="">
-                <h2 className={`${headStyle} text-center md:font-bold md:text-[40px]`}>Our core values</h2>
-                <section className="text-white grid md:gap-10 md:grid-cols-3">
+                <h2 className={`${headStyle} text-center font-bold md:font-bold md:text-[40px]`}>Our core values</h2>
+                <section className="text-white grid grid-cols-1 gap-5 md:gap-10 md:grid-cols-3">
                     <div className={`${card} bg-[#005c1d]`}>
                         <img className={`${cardImg}`} src={check} alt="" />
                         <p className={`${cardHead}`}>integrity</p>
