afd0a2ad3c9428104a918cff1b0d25761912bf2b
Salami Azeez <salamitopeazeez@gmail.com>
2024-11-18 17:29:30 +0100

    Adjusted imgHero Section

--- src/components/imgHero.jsx
+++ src/components/imgHero.jsx
@@ -5,7 +5,7 @@ import img3 from "../assets/img-3.png"
 export const ImgHero = () => {
     return (
         <section className="flex flex-col my-10 gap-10 bg-white p-5 rounded-2xl md:flex-row">
-            <div className="flex flex-col gap-10">
+            <div className="flex flex-col gap-5">
                 <div className="">
                     <img src={img1} alt="" />
                 </div>
