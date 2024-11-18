b9002f07bbc6d95089cf15849f06d4b925f5b093
Salami Azeez <salamitopeazeez@gmail.com>
2024-11-18 18:40:24 +0100

    Added id to productSection

--- src/components/productSection.jsx
+++ src/components/productSection.jsx
@@ -12,7 +12,7 @@ export const ProductSection = () => {
 
 
     return (
-        <section className="flex flex-col my-20 mx-10 gap-10 bg-white p-5  rounded-2xl md:mx-28">
+        <section id="products" className="flex flex-col my-20 mx-10 gap-10 bg-white p-5  rounded-2xl md:mx-28">
             <div className="text-center">
                 <h2 className={`${headStyle} font-bold md:text-[40px]`}>Products</h2>
             </div>
