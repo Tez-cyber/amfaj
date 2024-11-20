e8904b29c39b370ea8dc337b517694779428ab79
Salami Azeez <salamitopeazeez@gmail.com>
2024-11-20 02:05:15 +0100

    Added contactSection head

--- src/components/contactSection.jsx
+++ src/components/contactSection.jsx
@@ -1,7 +1,19 @@
 
 
 export const ContactSection = () => {
-  return (
-    <div>contactSection</div>
-  )
+    return (
+        <section id="services" className="flex flex-col justify-center items-center my-20 mx-10 gap-10 bg-white p-5 rounded-2xl md:px-8 md:mx-28">
+            <section className="text-center">
+                <h2 className={`text-[#016623] pb-6 text-[25px] font-bold leading-tight md:text-[40px]`}>
+                    We’d love to hear from you
+                </h2>
+            </section>
+            {/* ================= Form section ============= */}
+            <section className="">
+                <form className="">
+                    <label className={``}></label>
+                </form>
+            </section>
+        </section>
+    )
 }
