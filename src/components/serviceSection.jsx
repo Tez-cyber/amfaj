226785d6e6f5640dd7f9bfdc2bf4243e2817b0b7
Salami Azeez <salamitopeazeez@gmail.com>
2024-11-20 01:56:39 +0100

    Completed serviceSection

--- src/components/serviceSection.jsx
+++ src/components/serviceSection.jsx
@@ -1,9 +1,10 @@
+import { serviceCards } from "../utils"
 
 
 export const ServiceSection = () => {
     return (
-        <section id="services" className="flex flex-col my-20 mx-10 gap-10 bg-white p-5  rounded-2xl md:mx-28">
-            <div className="text-center">
+        <section id="services" className="flex flex-col my-20 mx-10 gap-10 bg-[#ffebe0] p-5 rounded-2xl md:px-8 md:mx-28">
+            <section className="text-center">
                 <h2 className={`text-[#016623] pb-6 text-[25px] font-bold leading-tight md:text-[40px]`}>
                     Expert Services <br className="hidden md:block" />
                     Tailored to Your Needs
@@ -13,8 +14,21 @@ export const ServiceSection = () => {
                     the diverse needs of our clients. Our dedicated team is committed to providing <br className="hidden md:block" />
                     exceptional support and expertise at every stage of our services.
                 </p>
-            </div>
+            </section>
             {/* ================= Card section ============= */}
+            <section className="grid gap-4 grid-cols-1 md:grid-cols-2 ">
+                {
+                    serviceCards.map((card) => (
+                        <section key={card.id} className="bg-white rounded-xl p-4">
+                            <img src={card.img} alt="" />
+                            <main className="pt-5">
+                                <h2 className="text-[#013010] text-lg md:text-[20px]">{card.title}</h2>
+                                <p className="pt-2 text-[13px] md:text-base">{card.content}</p>
+                            </main> 
+                        </section>
+                    ))
+                }
+            </section>
         </section>
     )
 }
