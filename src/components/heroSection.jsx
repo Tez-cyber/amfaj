f426d59f2323e961231c6cd9cb727729941b8116
Salami Azeez <salamitopeazeez@gmail.com>
2024-11-21 01:28:18 +0100

    Added image to navbar

--- src/components/heroSection.jsx
+++ src/components/heroSection.jsx
@@ -1,10 +1,16 @@
 // import left from "../assets/hero-left.jpeg"
 import { ImgHero } from "./imgHero"
 import { Partners } from "./partners"
+
 export const HeroSection = () => {
   return (
     <div id="home" className="mt-[100px] text-white md:gap-0 md:mt-0">
       <div className="text-center flex flex-col gap-5 items-center justify-center px-10 md:px-28">
+        <div className="-z-20">
+          <img src="/left-leaf.png" alt="" className="absolute w-[200px] top-[100px] left-0 md:w-auto" />
+          <img src="/right-leaf.png" alt="" className="absolute w-[200px] top-[100px] right-0 md:w-auto" />
+        </div>
+        <img src="" alt="" className="" />
         {/* ============== First Section =========== */}
         <h1 className="text-5xl font-bold md:leading-[88.56px] md:text-[70px] md:mt-6 ">
           Exceptional Agricultural <span className="hidden md:inline"><br /></span>
